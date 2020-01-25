addccc99bf9c169e35235bc6fd94f5c6b7c0c367
poignant-writer-1163 <edmundopierre@yahoo.com>
2020-01-25 15:14:45 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
+       
+         
+def my_collect(array)
+  counter = 0
+  name_collection = []
+  while counter < array.length
+    name_collection.push yield(array[counter])
+  
+    counter += 1
+  end
+  name_collection
+end
 
-
+my_collect(["Tim Jones", "Tom Smith", "Jim Campagno"]) {|counter| counter.split(" ").first}
