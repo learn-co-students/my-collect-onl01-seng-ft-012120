e32587130c99e786d43eae70374c60107eff7013
Ramesh Chuwan <chhetrirahul2015@gmail.com>
2020-01-27 05:38:25 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,14 @@
-
+def my_collect(array)
+  first_names = []
+  if block_given?
+    i = 0 
+    while i < array.length do 
+      first_names << yield(array[i])
+      i += 1 
+    end
+  else
+    "NO! block is given!"
+  end
+  return first_names
+end
 
