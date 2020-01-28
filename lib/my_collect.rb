653df17526b9125e798d14f1847f2d01937dd337
Angela Appiah <alegnalawn2@yahoo.com>
2020-01-28 06:20:26 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,12 @@
+def students_name
+  name = ["Tim Jones", "Tom Smith", "Jim Campagno"]
+my_collect(array) do |name|
+  name.split(" ").first
+end
 
+def new collection
+  collection = ['ruby', 'javascript', 'python', 'objective-c']
+my_collect(collection) do |lang|
+  lang.upcase
+end 
 
